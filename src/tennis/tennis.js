2f339815df96ee77eed3ed115a2615094eb06045
Nataly <nramirezmachicado@gmail.com>
2026-02-26 10:22:27 -0400

    feat: el jugador 1 gana 3 puntos y jugador 2 esta en 0

--- src/tennis/tennis.js
+++ src/tennis/tennis.js
@@ -10,6 +10,9 @@ class Tennis{
         if (this.player1points === 2) {
             player1Score = "30 ";
         }
+        if (this.player1points === 3) {
+            player1Score = "40 ";
+        }
         return player1Score + "- Love";
     }
 
--- src/tennis/tennis.spec.js
+++ src/tennis/tennis.spec.js
@@ -19,6 +19,22 @@ describe("tennis", () => {
         expect(tennis.score()).toEqual("30 - Love");
     });
 
+    it("Jugador 1 anota 3 puntos", () => {
+       let tennis = new Tennis ();
+        tennis.player1Scores();
+        tennis.player1Scores();
+        tennis.player1Scores();
+        expect(tennis.score()).toEqual("40 - Love");
+    });
+
+    // it("Jugador 1 anota 4 puntos", () => {
+    //      let tennis = new Tennis ();
+    //     tennis.player1Scores();
+    //     tennis.player1Scores();
+    //     tennis.player1Scores();
+    //     tennis.player1Scores();
+    //     expect(tennis.score()).toEqual("Game for Player 1");
+    // });
 });
 
 
