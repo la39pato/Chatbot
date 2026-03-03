ec7da9e652c0586f326a4dc29aa759c956ad1d93
Nataly <nramirezmachicado@gmail.com>
2026-03-03 00:16:51 -0400

    refac: buscamos la forma de optimizar nuestro codigo de tennis.js

--- src/tennis/tennis.js
+++ src/tennis/tennis.js
@@ -1,63 +1,33 @@
-class Tennis{
+class Tennis {
     constructor() {
         this.player1points = 0;
         this.player2points = 0;
     }
+
     score() {
-        /*---Ganan el juego---*/
-        if (this.player1points >= 4 && this.player1points >= this.player2points + 2) {
-            return "Game for Player 1";
+        const diferenciascore = this.player1points - this.player2points;
+
+        // Game
+        if (this.player1points >= 4 || this.player2points >= 4) {
+            if (diferenciascore >= 2) return "Game for Player 1";
+            if (diferenciascore <= -2) return "Game for Player 2";
+            if (diferenciascore === 1) return "Advantage for Player 1";
+            if (diferenciascore === -1) return "Advantage for Player 2";
         }
 
-        if (this.player2points >= 4 && this.player2points >= this.player1points + 2) {
-            return "Game for Player 2";
-        }
-
-        // --- CASO EMPATE (DEUCE) ---
-        if (this.player1points === this.player2points && this.player1points >= 3) {
+        // Deuce
+        if (this.player1points >= 3 && this.player1points === this.player2points) {
             return "Deuce";
         }
-        if (this.player1points >= 4 && this.player1points === this.player2points + 1) {
-            return "Advantage for Player 1";
-        }
-        if (this.player2points >= 4 && this.player2points === this.player1points + 1) {
-            return "Advantage for Player 2";
-        }
-        
-        /*---Ganan 1 punto---*/
-        let player1Score = "Love ";
-        if (this.player1points === 1) {
-            player1Score = "15 ";
-        }
-        let player2Score = " Love";
-        if(this.player2points === 1) {
-            player2Score = " 15";
-        }
 
-        /*---Ganan 2 puntos---*/
-        if (this.player1points === 2) {
-            player1Score = "30 ";
-        }
-        if (this.player2points === 2) {
-            player2Score = " 30";
-        }
-        
-        /*---Ganan 3 puntos---*/
-        if (this.player1points === 3) {
-            player1Score = "40 ";
-        }
-        if (this.player2points === 3) {
-            player2Score = " 40";
-        }
-
-        
-
-        return player1Score + "-" + player2Score;
+        const scores = ["Love", "15", "30", "40"];
+        return scores[this.player1points] + " - " + scores[this.player2points];
     }
 
     player1Scores() {
         this.player1points++;
     }
+
     player2Scores() {
         this.player2points++;
     }
